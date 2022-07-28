043b6690136110bd68c597ec931f902fda3f0b1c
William Niemiec <williamniemiec@hotmail.com>
2022-07-28 14:21:13 -0300

    Tests

--- test/wniemiec/util/cpp/StringUtilsTest.cpp
+++ test/wniemiec/util/cpp/StringUtilsTest.cpp
@@ -4,93 +4,116 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <vector>
+#include "../../../../include/wniemiec/util/cpp/StringUtils.hpp"
 
 using namespace wniemiec::util::cpp;
+
+//-----------------------------------------------------------------------------
+//		Test hooks
+//-----------------------------------------------------------------------------
+void StringUtilsTest::set_up() 
+{
+    selected_string = std::string();
+    obtained_result = std::string();
+    obtained_terms = std::vector<std::string>();
+}
+
 
 //-----------------------------------------------------------------------------
 //		Tests
 //-----------------------------------------------------------------------------
 TEST_F(StringUtilsTest, test_split_with_two_terms)
 {
-    std::string str = "hello world "; 
-    std::string sep = " ";
-    std::vector<std::string> expectedTerms = std::vector<std::string>();
-    expectedTerms.push_back("hello");
-    expectedTerms.push_back("world");
-    std::vector<std::string> terms = StringUtils::split(str, sep);
-    EXPECT_EQ(
-        expectedTerms,
-        terms
-    );
+    with_str("hello world");
+    do_split_with_sep("  ");
+    assert_terms_are({
+        "hello",
+        "world"
+    });
 }
 
 TEST_F(StringUtilsTest, test_split_with_three_terms)
 {
-    std::string str = "hello world two"; 
-    std::string sep = " ";
-    std::vector<std::string> expectedTerms = std::vector<std::string>();
-    expectedTerms.push_back("hello");
-    expectedTerms.push_back("world");
-    expectedTerms.push_back("two");
-    std::vector<std::string> terms = StringUtils::split(str, sep);
-    EXPECT_EQ(
-        expectedTerms,
-        terms
-    );
+    with_str("hello world two");
+    do_split_with_sep(" ");
+    assert_terms_are({
+        "hello",
+        "world",
+        "two"
+    });
 }
 
 TEST_F(StringUtilsTest, test_split_with_separator_size_two)
 {
-    std::string str = "hello  world"; 
-    std::string sep = "  ";
-    std::vector<std::string> expectedTerms = std::vector<std::string>();
-    expectedTerms.push_back("hello");
-    expectedTerms.push_back("world");
-    std::vector<std::string> terms = StringUtils::split(str, sep);
-    EXPECT_EQ(
-        expectedTerms,
-        terms
-    );
+    with_str("hello  world");
+    do_split_with_sep("  ");
+    assert_terms_are({
+        "hello",
+        "world"
+    });
 }
 
 TEST_F(StringUtilsTest, test_split_with_empty_str)
 {
-    std::string str = ""; 
-    std::string sep = " ";
-    std::vector<std::string> expectedTerms = std::vector<std::string>();
-    expectedTerms.push_back("");
-    std::vector<std::string> terms = StringUtils::split(str, sep);
-    EXPECT_EQ(
-        expectedTerms,
-        terms
-    );
+    with_str("");
+    do_split_with_sep(" ");
+    assert_terms_are({""});
 }
 
 TEST_F(StringUtilsTest, test_replace_all_with_one_value_and_new_value_with_size_one)
 {
-    std::string str = "hello world";
-    std::string expected_str = "hello-world";
-    std::string old_value = " ";
-    std::string new_value = "-";
-    std::string obtained_str = StringUtils::replace_all(str, old_value, new_value);
-
-    EXPECT_STREQ(
-        expected_str.c_str(),
-        obtained_str.c_str()
-    );
+    with_str("hello world");
+    do_replace_all_with_old_and_new_value(" ", "-");
+    assert_result_is("hello-world");
 }
 
 TEST_F(StringUtilsTest, test_replace_all_with_one_value_and_new_value_with_size_two)
 {
-    std::string str = "hello world";
-    std::string expected_str = "hello--world";
-    std::string old_value = " ";
-    std::string new_value = "--";
-    std::string obtained_str = StringUtils::replace_all(str, old_value, new_value);
+    with_str("hello world");
+    do_replace_all_with_old_and_new_value(" ", "--");
+    assert_result_is("hello--world");
+}
 
+
+//-----------------------------------------------------------------------------
+//		Methods
+//-----------------------------------------------------------------------------
+void StringUtilsTest::with_str(std::string str)
+{
+    selected_string = str;
+}
+
+void StringUtilsTest::do_split_with_sep(std::string sep)
+{
+    obtained_terms = StringUtils::split(selected_string, sep);
+}
+
+void StringUtilsTest::assert_terms_are(std::vector<std::string> expected_terms)
+{
+    EXPECT_EQ(expected_terms, obtained_terms);
+}
+
+void StringUtilsTest::do_replace_all_with_old_and_new_value(
+    std::string old_value, 
+    std::string new_value
+)
+{
+    obtained_result = StringUtils::replace_all(
+        selected_string, 
+        old_value, 
+        new_value
+    );
+
+    
+}
+
+void StringUtilsTest::assert_result_is(std::string expected_result)
+{
     EXPECT_STREQ(
-        expected_str.c_str(),
-        obtained_str.c_str()
+        expected_result.c_str(),
+        obtained_result.c_str()
     );
 }
 
+   
--- test/wniemiec/util/cpp/StringUtilsTest.hpp
+++ test/wniemiec/util/cpp/StringUtilsTest.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
 #include <gtest/gtest.h>
-#include "../../../../include/wniemiec/util/cpp/StringUtils.hpp"
+#include <iostream>
+#include <vector>
 
 namespace wniemiec { namespace util { namespace cpp {
 
@@ -10,21 +11,31 @@ namespace wniemiec { namespace util { namespace cpp {
     //-------------------------------------------------------------------------
     //		Attributes
     //-------------------------------------------------------------------------
-    //private:
-        //DotExport dotExport;
+    private:
+        std::string selected_string;
+        std::string obtained_result;
+        std::vector<std::string> obtained_terms;
 
 
     //-------------------------------------------------------------------------
-    //		Constructor
+    //		Test hooks
     //-------------------------------------------------------------------------
-    // public:
-    //     DotExportTest();
+    protected:
+        void SetUp() override { set_up(); };
+        void set_up();
 
 
     //-------------------------------------------------------------------------
     //		Methods
     //-------------------------------------------------------------------------
-    //protected:
-        
+    protected:
+        void with_str(std::string str);    
+        void do_split_with_sep(std::string sep);
+        void assert_terms_are(std::vector<std::string> expected_terms);
+        void do_replace_all_with_old_and_new_value(
+            std::string old_value, 
+            std::string new_value
+        );
+        void assert_result_is(std::string expected_result);
     };
 }}}
